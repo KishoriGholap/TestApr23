b8ea6177feb10ac73cff2ce1f98922f043a3f2db
KishoriGholap <kishori.gholap@gmail.com>
2023-05-23 08:29:36 +0530

    added lines in b branch

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -1,5 +1,9 @@
 package basicConcept;
 
 public class StringPrac {
-
+	public static void main (String [] args) {
+	
+System.out.println("creating conflict in B");
+System.out.println("new msg");
+	}
 }
