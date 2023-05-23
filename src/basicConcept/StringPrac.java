0dc41ab9ef59f4ea60d7d6c88c7131730c452b0d
KishoriGholap <kishori.gholap@gmail.com>
2023-05-23 09:47:39 +0530

    New chnages in A3

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -2,7 +2,8 @@ package basicConcept;
 
 public class StringPrac {
 	public static void main(String[] args) {
-System.out.println("This is branch B2 changes to create conflict");
+		System.out.println("This is branch B2 changes to create conflict");
+		System.out.println("This is for scenario2 conflict");
 
-}
+	}
 }
