86d8a77467c47cbd3fadd7a0ca1e679a427db6a8
KishoriGholap <kishori.gholap@gmail.com>
2023-05-23 09:51:31 +0530

    Commit form B3

--- src/basicConcept/StringPrac.java
+++ src/basicConcept/StringPrac.java
@@ -3,5 +3,6 @@ package basicConcept;
 public class StringPrac {
 public static void main (String[] args) {
 	System.out.println("chnages done in A2");
+	System.out.println("This is for branch b3 chnages for conflcit");
 }
 }
